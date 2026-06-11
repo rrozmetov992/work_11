38ad65bd417ad4e60031cd13f4fdfa3d38b9aaaa
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:15:29 +0300

    Add files via upload

---
+++ work_11.py
@@ -0,0 +1,42 @@
+#Boolean 3
+# a=int(input())
+# b=a%2==0
+# c=a>0
+# print(b,c)
+#Boolean 4
+# A=int(input())
+# B=int(input())
+# a=A>2 and B<=3
+# print(a)
+#Boolean 5
+# A=int(input())
+# B=int(input())
+# a=A>=0 or B< -2
+# print(a)
+#Boolean 6
+# A=int(input())
+# B=int(input())
+# C=int(input())
+# a=A<=B<=C
+# print(a)
+#Boolean 7
+# A=int(input())
+# B=int(input())
+# C=int(input())
+# a=A<=B and B<=C
+# print(a)
+#Boolean 8
+# A=int(input())
+# B=int(input())
+# a=A%2==1 and B%2==1
+# print(a)
+#Boolean 9
+# A=int(input())
+# B=int(input())
+# a=A%2==1 or B%2==1
+# print(a)
+#Boolean 10
+# A=int(input())
+# B=int(input())
+# a=A%2==1 and B%2==0 or A%2==0 and B%2==1
+# print(a)
